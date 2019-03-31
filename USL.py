cb5593c9b2808698aef95ff456d3fa2b97494c92
wip727 <wip727@gmail.com>
2019-03-30 23:54:35 -0400

    draft up USL

--- USL.py
+++ USL.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as pyplot
+from scipy.optimize import curve_fit
 
 class USL:
     # Attributes:
@@ -9,8 +11,21 @@ class USL:
     # Coherency
     beta = 0
 
+    # Raw data to fit
+    rawx = None
+    rawy = None
+
     # Initializer, with initial guess as optional inputs
-    def __init__(self, gamma0=1, alpha0=0, beta0=0):
+    def __init__(self, gamma0 = 1, alpha0 = 0, beta0 = 0):
         self.gamma = gamma0
         self.alpha = alpha0
         self.beta = beta0
+
+    # Fit command, estimate the parameters of USL using nonlinear least square
+    def fit(self, x = None, y = None):
+        if (x is not None) && (y is not None):
+            self.rawx = x
+            self.rawy = y
+
+            # Fit the data
+            xgrid = np.linspace(min(self.rawx), max(self.rawx), len(rawx))
